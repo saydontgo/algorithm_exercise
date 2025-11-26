1e15dad245ae98c4798e8f010866da2ee5f3c7c1
zjf <liu358813@gmail.com>
2025-11-26 12:14:34 +0800

    leetcode problem 239 solved - 加入单调队列的做法

--- leetcode/239/239.cpp
+++ leetcode/239/239.cpp
@@ -46,34 +46,66 @@ void addele(deque<pair<int, int>>& heap) {
     }
 }
 
+// 最基础的堆写法
+// vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//     vector<int> res;
+//     deque<pair<int, int>> heap;
+
+//     for (int i = 0; i < k; i++) {
+//         heap.push_back({nums[i], i});
+//         addele(heap);
+//     }
+
+//     res.push_back(heap[0].first);
+//     for (int i = k; i < nums.size(); i++) {
+//         if (heap[0].second < i - k + 1) {
+//             heap[0] = {nums[i], i};
+//             heapify(heap);
+//             while (heap[0].second < i - k + 1) {
+//                 heap[0] = heap[heap.size() - 1];
+//                 heap.pop_back();
+//                 heapify(heap);
+//             }
+//         } else {
+//             if (heap[0].first < nums[i]) {
+//                 heap[0] = {nums[i], i};
+//             } else {
+//                 heap.push_back({nums[i], i});
+//                 addele(heap);
+//             }
+//         }
+//         res.push_back(heap[0].first);
+//     }
+//     return res;
+// }
+
+// 单调队列的做法
 vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    deque<pair<int, int>> window;
     vector<int> res;
-    deque<pair<int, int>> heap;
-
-    for (int i = 0; i < k; i++) {
-        heap.push_back({nums[i], i});
-        addele(heap);
-    }
-
-    res.push_back(heap[0].first);
-    for (int i = k; i < nums.size(); i++) {
-        if (heap[0].second < i - k + 1) {
-            heap[0] = {nums[i], i};
-            heapify(heap);
-            while (heap[0].second < i - k + 1) {
-                heap[0] = heap[heap.size() - 1];
-                heap.pop_back();
-                heapify(heap);
-            }
-        } else {
-            if (heap[0].first < nums[i]) {
-                heap[0] = {nums[i], i};
-            } else {
-                heap.push_back({nums[i], i});
-                addele(heap);
-            }
+    window.push_back({nums[0], 0});
+    for (int i = 1; i < k; i++) {
+        auto size = window.size();
+        while (window[size - 1].first <= nums[i]) {
+            window.pop_back();
+            size--;
+            if (size == 0) { break; }
         }
-        res.push_back(heap[0].first);
+        window.push_back({nums[i], i});
+    }
+    res.push_back(window[0].first);
+
+    for (int i = k; i < nums.size(); i++) {
+        auto size = window.size();
+        while (size > 0 && window[0].second < i - k + 1) {
+            window.pop_front(); 
+            --size; 
+        }
+        while (size > 0 && window[--size].first <= nums[i]) {
+            window.pop_back();
+        }
+        window.push_back({nums[i], i});
+        res.push_back(window[0].first);
     }
     return res;
 }
