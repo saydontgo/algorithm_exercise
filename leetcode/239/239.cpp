ea217a514ac424f0444180728f183b29ce5259a6
zjf <liu358813@gmail.com>
2025-11-26 11:09:56 +0800

    leetcode problem 239 solved

---
+++ leetcode/239/239.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+
+void heapify(deque<pair<int, int>>& heap) {
+    int index = 0;
+    auto size = heap.size();
+    while (true) {
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+        int max_i = -1e5;
+        if (left < size && right < size) {
+            max_i = heap[left].first > heap[right].first ? left : right;
+        } else if (left < size) {
+            max_i = left;
+        } else if (right < size) {
+            max_i = right;
+        } else {
+            return;
+        }
+
+        if (heap[max_i].first > heap[index].first) {
+            pair<int, int> tmp = heap[index];
+            heap[index] = heap[max_i];
+            heap[max_i] = tmp;
+        } else {
+            return;
+        }
+        index = max_i;
+    }
+}
+
+void addele(deque<pair<int, int>>& heap) {
+    int index = heap.size() - 1;
+    while (true) {
+        int father = index & 1 ? (index - 1) / 2 : (index - 2) / 2;
+
+        if (father >= 0 && heap[father].first < heap[index].first) {
+            pair<int, int> tmp = heap[index];
+            heap[index] = heap[father];
+            heap[father] = tmp;
+        } else { return; }
+
+        index = father;
+    }
+}
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> res;
+    deque<pair<int, int>> heap;
+
+    for (int i = 0; i < k; i++) {
+        heap.push_back({nums[i], i});
+        addele(heap);
+    }
+
+    res.push_back(heap[0].first);
+    for (int i = k; i < nums.size(); i++) {
+        if (heap[0].second < i - k + 1) {
+            heap[0] = {nums[i], i};
+            heapify(heap);
+            while (heap[0].second < i - k + 1) {
+                heap[0] = heap[heap.size() - 1];
+                heap.pop_back();
+                heapify(heap);
+            }
+        } else {
+            if (heap[0].first < nums[i]) {
+                heap[0] = {nums[i], i};
+            } else {
+                heap.push_back({nums[i], i});
+                addele(heap);
+            }
+        }
+        res.push_back(heap[0].first);
+    }
+    return res;
+}
+int main() {
+    return 0;
+}
