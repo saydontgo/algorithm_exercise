ab3bbb4d5f013af77da72dd27273ebd6caf8fe3c
zjf <liu358813@gmail.com>
2026-01-05 09:25:35 +0800

    leetcode problem 279 solved

---
+++ leetcode/279/279.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int numSquares(int n) {
+        int dp[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            for (int j = 2; j * j <= i; j++) {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
