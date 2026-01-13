2e8e75696b607ae59f8366efd0e76c1542b2f4a7
zjf <liu358813@gmail.com>
2026-01-13 21:21:56 +0800

    leetcode problem 48 solved

---
+++ leetcode/48/48.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        int layer = n / 2 + 1;
+
+        for (int i = 0; i < layer; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j -1];
+                matrix[n - i - 1][n - j -1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = tmp;
+            }
+        }
+    }
+};
